1785971234d10e7e880c584fa505c06730f84eef
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2019-05-11 20:32:40 +0300

    Add files via upload

---
+++ precache-manifest.15b7604c61a7e3750565b9f36ce6ca0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c994587d2e28732d43057ec8a917fb06",
+    "url": "/qoutes/index.html"
+  },
+  {
+    "revision": "8a20ba7be94f0c4759af",
+    "url": "/qoutes/static/css/main.96b9e37b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/qoutes/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "8a20ba7be94f0c4759af",
+    "url": "/qoutes/static/js/main.ca21271f.chunk.js"
+  },
+  {
+    "revision": "b23e0a98974439787cbf",
+    "url": "/qoutes/static/js/runtime~main.edc97e64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/IlyaIzr/qoutes/precache-manifest.8e398e121dbf6ba97cbd16e3a9fdd2e0.js"
+  "/qoutes/precache-manifest.15b7604c61a7e3750565b9f36ce6ca0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IlyaIzr/qoutes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qoutes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
