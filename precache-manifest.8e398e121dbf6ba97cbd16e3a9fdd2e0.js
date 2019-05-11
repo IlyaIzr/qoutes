96aa33e272619fe4ff0988fc31442195231de5d7
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2019-05-11 20:29:00 +0300

    Add files via upload

---
+++ precache-manifest.8e398e121dbf6ba97cbd16e3a9fdd2e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29010df2cfd9a4397c43f61dee7c5df3",
+    "url": "/IlyaIzr/qoutes/index.html"
+  },
+  {
+    "revision": "aba4ffbcb2cbe9eb9c2d",
+    "url": "/IlyaIzr/qoutes/static/css/main.96b9e37b.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/IlyaIzr/qoutes/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "aba4ffbcb2cbe9eb9c2d",
+    "url": "/IlyaIzr/qoutes/static/js/main.a13b359b.chunk.js"
+  },
+  {
+    "revision": "47bf751904e9d2232a25",
+    "url": "/IlyaIzr/qoutes/static/js/runtime~main.21683fd8.js"
+  }
+]);
